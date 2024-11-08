74a35c32f8eb697e0fd4a03e67fc96d24424d391
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-08 02:11:51 -0300

    [ :sparkles: ] | create: entity - event (module::domain)

---
+++ CaramelMQ/src/domain/entity/event.rs
@@ -0,0 +1,17 @@
+pub struct Event<T> {
+    pub id: u64,
+    pub payload: T,
+    pub timestamp: i64,
+    pub topic: String,
+}
+
+impl<T> Event<T> {
+    pub fn new(id: u64, payload: T, timestamp: i64, topic: String) -> Self {
+        Event {
+            id,
+            payload,
+            timestamp,
+            topic,
+        }
+    }
+}
