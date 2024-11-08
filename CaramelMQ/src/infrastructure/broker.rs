058e1198d8571b970fd9488aa8641a0c3d9fca43
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-08 03:22:48 -0300

    [ :building_construction: ] | implements: broker - message > start (module::infra)

--- CaramelMQ/src/infrastructure/broker.rs
+++ CaramelMQ/src/infrastructure/broker.rs
@@ -1,15 +1,40 @@
-use std::sync::Arc;
+use std::{
+    sync::Arc,
+    thread::{sleep, spawn},
+};
 
 use crate::domain::{
-    entity::{broker::MessageBroker, queue::Queue},
+    entity::{broker::MessageBroker, event::Event, queue::Queue},
     service::service::EventService,
 };
 
-impl<T: Send + Sync + 'static> MessageBroker<T> {
+impl<T: Clone + Send + Sync + 'static> MessageBroker<T> {
     pub fn new(event_service: Arc<EventService<T>>) -> Self {
         Self {
             event_service,
             queue: Queue::new(),
         }
+    }
+
+    pub fn start(&self) {
+        let queue = self.queue.clone();
+        let service = self.event_service.clone();
+
+        spawn(move || loop {
+            if let Some(event) = queue.dequeue() {
+                let listeners = service.listeners.lock().unwrap();
+
+                if let Some(listener_group) = listeners.get(&event.id) {
+                    for listener in listener_group {
+                        listener.on_event(&event);
+                    }
+                }
+            }
+            sleep(std::time::Duration::from_millis(10)); // Tunable to optimize performance
+        });
+    }
+
+    pub fn enqueue(&self, event: Event<T>) {
+        self.queue.enqueue(event);
     }
 }
